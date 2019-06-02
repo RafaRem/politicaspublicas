583b4fc01994a23d02731f7df61a66972b255789
clioo <jesus_acosta1996@hotmail.com>
2019-06-01 19:50:27 -0600

    cambio de staticfiles otra vez

--- seguimientometas/settings.py
+++ seguimientometas/settings.py
@@ -142,6 +142,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
