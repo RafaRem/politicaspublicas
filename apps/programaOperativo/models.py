4b9e158a5ca492134ff4c3463fe20f2aa7b91d44
clioo <jesus_acosta1996@hotmail.com>
2019-06-07 10:52:41 -0600

    cambio en base de datos, programas operativos

--- apps/programaOperativo/models.py
+++ apps/programaOperativo/models.py
@@ -36,15 +36,6 @@ class ProgramaOperativo(models.Model):
         ('i','Inactivo')
     )
     nombre = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Descripción')
-    tipoPrograma = models.CharField(max_length=30,choices=opcionesTipoPrograma, blank=True, null=True, 
-    verbose_name='Tipo de programa')
-    objetivos = models.CharField(max_length=1000,blank=True, null=True, verbose_name='Objetivos')
-    estrategias = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Estrategias')
-    beneficiarios = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Beneficiarios')
-    justificacion = models.CharField(max_length=1000, blank=True, null=True, verbose_name='Justificación')
-    problematicaSocial = models.CharField(max_length=1000, blank=True, null=True, verbose_name = 'Problemática social')
-    #BORRAR OBJETIVO AL MANDAR A PRODUCCION
     objetivo = models.ForeignKey(Objetivo, on_delete=models.PROTECT)
     dependencia = models.ForeignKey(Dependencia, on_delete=models.PROTECT)
     acciones = models.ManyToManyField(Acciones, blank=True)
