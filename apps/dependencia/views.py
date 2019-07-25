678662171d0cce1cd029092cd5101e44bb7473bc
clioo <jesus_acosta1996@hotmail.com>
2019-07-25 14:29:58 -0600

    Listas las asignaciones a departamentos

--- apps/dependencia/views.py
+++ apps/dependencia/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, reverse
 from django.core import serializers
+from django.contrib import messages
 """Models"""
 from apps.programaOperativo.models import ProgramaOperativo
 from apps.dependencia.models import Departamento
@@ -12,14 +13,29 @@ from apps.dependencia.models import Departamento
 class ProgramasOperativosList(LoginRequiredMixin,View):
     """Will show all the department's operative programs"""
     login_url = 'login'
-    def get(self,request):
+    def get_context(self,request):
         programasOperativos = ProgramaOperativo.objects.filter(estado='a',dependencia=request.user.profile.dependencia)
         programasOperativos = programasOperativos.order_by('nombre')
         departamentos = Departamento.objects.filter(dependencia=request.user.profile.dependencia)
         departamentos = departamentos.order_by('nombre')
         departamentos = serializers.serialize('json',departamentos)
-        return render(request,'dependencias/programasoperativos.html',{
+        return {
             'programasOperativos':programasOperativos,
             'departamentos':departamentos
-        })
-
+        }
+    def get(self,request):
+        return render(request,'dependencias/programasoperativos.html',self.get_context(request))
+    def post(self,request):
+        #values comes separated by | where the first element is the operative program and the second is the department 
+        valoresPo_Dep = request.POST.getlist('valoresDependencia')
+        for valorPo_Dep in valoresPo_Dep:
+            #0 value is the operative program's id and 1 is the department's id
+            arrValor = valorPo_Dep.split('|')
+            programaOperativo = ProgramaOperativo.objects.get(pk=arrValor[0])
+            departamento = Departamento.objects.get(pk=arrValor[1])
+            programaOperativo.departamento = departamento
+            programaOperativo.save()
+            pass
+        messages.success(request,'Departamentos cambiados con éxito')
+        return render(request,'dependencias/programasoperativos.html',self.get_context(request))        
+    
