1a5cfb142e6d478869f71c8c23947d392fe10755
clioo <jesus_acosta1996@hotmail.com>
2019-11-14 13:21:30 -0700

    asdasd

--- apps/indicador/views.py
+++ apps/indicador/views.py
@@ -776,7 +776,7 @@ class FichaProgramaOperativo(LoginRequiredMixin,View):
 class FichaDependencia(LoginRequiredMixin,View):
     login_url = 'login'
     def get(self,request, idDependencia, idPeriodoGobierno="0"):
-      
+        print(idPeriodoGobierno)
         if (str(request.user.profile.dependencia.id) != str(idDependencia)) and ((request.user.profile.tipoUsuario != 'a') and (request.user.profile.tipoUsuario != 's') and (request.user.profile.tipoUsuario != 'i')):
             return redirect('index')
         porcentajeMeta = PorcentajesMetas()
